1def3465e389d7dfece9c102d6f96ce190440606
Natalia <aniskinan@internet.ru>
2022-09-26 19:31:10 +0500

    добавила лекцию

---
+++ lec.py
@@ -0,0 +1,80 @@
+print ('hello world')
+a = 123
+b = 1,23
+print (a)
+print (b)
+value = None
+value = 12334
+print ( type (value))
+s= 'hello world'
+print (a, ' - ', b, ' - ', s)
+print ('{} - {} - {}'. format(a,  b,  s))
+print (f'{a} - {b} - {s}')
+f=True
+print(f)
+list = [1,2,3]
+col = ['hello', 1, 345, True]
+print (list)
+print (col)
+
+print ('Введите z')
+z= input()
+print (z)
+
+print ('Введите k');
+k=float (input())
+print(a+a)
+a=1.31221154
+b=-3
+c=round(a*b,7)
+print(c)
+d=a/b
+print(d)
+g=3
+g*=5
+print(g)
+l=1>4 and 3<5
+print (l)
+n='go'
+m='go'
+print (n==m)
+v=1 >2 or 4<6
+print (v)
+p=[1,2,3,4,]
+print(p)
+print (not 2 in p)
+# is_odd = f[0] % 2==0
+# print (is_odd)
+a=int(input())
+b=int(input())
+if a>b:
+    print(a)
+else:
+    print(b)
+    
+original=23
+inverted =0
+while original !=0:
+    inverted =inverted*10 + (original % 10)
+    original //=10
+
+else:
+    print('пожалуй')
+    print('хватит')
+print(inverted)
+for i in 1,2,3,4,5:
+   print (i)
+
+u=range(10)
+for i in u:
+    print(i)
+
+def f(x):
+    if x==1:
+        return 'целое'
+    elif x==2.3:
+        return 23
+    else:
+        return
+arg=1
+print(f(arg))
